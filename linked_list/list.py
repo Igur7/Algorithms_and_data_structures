5a7342d267ee9bc8351836a4d542c2b52a9fd9af
igur7 <igoroleksiewicz9@gmail.com>
2026-03-10 16:17:41 +0100

    linked list implementation

---
+++ linked_list/list.py
@@ -0,0 +1,77 @@
+            
+class node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class linked_list:
+    def __init__(self):
+        self.head = None
+
+    def destroy(self):
+        self.head = None
+
+    def add(self,data):
+        new_node = node(data)
+        if self.head is None:
+            self.head = new_node
+            new_node.next = None
+            return
+        last_node = self.head
+        self.head = new_node
+        new_node.next = last_node
+
+    def append(self,data):
+        new_node = node(data)
+        if self.head is None:
+            self.head = new_node
+            new_node.next = None
+            return
+        last_node = self.head 
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+        new_node.next = None
+    
+    def remove(self):
+        if self.head is None:
+            return 
+        if self.head.next is None:
+            self.head = None
+            return
+        self.head = self.head.next
+
+    def remove_end(self):
+        if self.head is None:
+            return 
+        if self.head.next is None:
+            self.head = None
+            return
+        last_node = self.head
+        while last_node.next.next:
+            last_node = last_node.next
+        last_node.next = None
+
+    def is_empty(self):
+        return self.head is None
+
+    def length(self):
+        count = 0
+        current_node = self.head
+        while current_node:
+            count += 1
+            current_node = current_node.next
+        return count    
+
+    def get(self):
+        if self.head is None:
+            return None
+        return self.head.data
+
+
+    def print_list(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data)
+            current_node = current_node.next 
+        
---
+++ linked_list/main.py
@@ -0,0 +1,40 @@
+import list
+
+unis = [('AGH', 'Kraków', 1919),
+('UJ', 'Kraków', 1364),
+('PW', 'Warszawa', 1915),
+('UW', 'Warszawa', 1915),
+('UP', 'Poznań', 1919),
+('PG', 'Gdańsk', 1945)]
+
+uczelnie = list.linked_list()
+
+uczelnie.append(unis[0])
+uczelnie.append(unis[1])
+uczelnie.append(unis[2])
+
+uczelnie.add(unis[3])
+uczelnie.add(unis[4])
+uczelnie.add(unis[5])
+
+uczelnie.print_list()
+
+print(uczelnie.length())
+
+uczelnie.remove()
+
+print("Po usunięciu pierwszego elementu:")
+print(uczelnie.get())
+
+uczelnie.remove_end()
+
+uczelnie.print_list()
+
+uczelnie.destroy()
+print(uczelnie.is_empty())
+
+uczelnie.remove()
+uczelnie.remove_end()
+uczelnie.add(unis[0])
+uczelnie.remove_end()
+print(uczelnie.is_empty())  
